16dc35c91a0df99ca80f935ab9a8c50d60d2c4dc
Vanessa <87830671+VansPhilip@users.noreply.github.com>
2021-07-26 14:00:49 +0400

    Update ChessEngine.py

--- Chess/Chess/ChessEngine.py
+++ Chess/Chess/ChessEngine.py
@@ -16,4 +16,36 @@ class GameState:
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+# VP        
+        self.WhiteToMove = True
+        self.moveLog = []
 
+    def makeMove(self, move):
+        if self.board[move.startrow][move.startcol] != '--':
+            self.board[move.startrow][move.startcol] = "--"
+            self.board[move.endrow][move.endcol] = move.piecemoved
+            self.moveLog.append(move)
+            self.WhiteToMove = not self.WhiteToMove
+
+class Move():
+
+    rankstoRows = {"1":7, "2":6, "3":5, "4":4, "5":3, "6":2, "7":1, "8":0}
+    rowstoRanks = {v:k for k, v in rankstoRows.items()}
+
+    filestoCol = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7}
+    coltoFiles = {v:k for k, v in filestoCol.items()}
+
+    def __init__(self, startsq, endsq, board):
+        self.startrow = startsq[0]
+        self.startcol = startsq[1]
+        self.endrow = endsq[0]
+        self.endcol = endsq[1]
+        self.piecemoved = board[self.startrow][self.startcol]
+        self.piececaptured = board[self.endrow][self.endcol]
+
+    def getRankFile(self, r, c):
+        return self.coltoFiles[c] + self.rowstoRanks[r]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startrow, self.startcol) + self.getRankFile(self.endrow, self.endcol) 
+    
