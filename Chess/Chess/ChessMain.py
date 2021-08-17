ca9bcf870d2a3346eb1181a41c23dbf44fba1aec
thea02 <87471731+thea02@users.noreply.github.com>
2021-08-17 12:43:56 +0530

    Update ChessMain.py

--- Chess/Chess/ChessMain.py
+++ Chess/Chess/ChessMain.py
@@ -18,7 +18,7 @@ def loadImages():
    pieces= ["wp", "wR", "wN","wB", "wK", "wQ", "bp", "bR", "bN", "bB", "bK", "bQ"]
    for piece in pieces:
        IMAGES[piece] = pygame.transform.scale(pygame.image.load('images/' + piece + '.png'), (SQ_SIZE, SQ_SIZE))
-
+         
 # the main driver for our code which will handle user input and uploading graphics
 def main():
 
@@ -33,6 +33,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+       
+          
 # VP                
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 location = pygame.mouse.get_pos()
@@ -50,12 +52,92 @@ def main():
                     gs.makeMove(move)
                     sqSelected = ()
                     playerclicks= []
-# TCA
-        drawGameState(screen, gs)
+#TCA                     
+        main_menu()
         clock.tick(MAX_FPS)
-        pygame.display.flip()
+        pygame.display.flip() 
+                     
+#TCA
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
 
-def drawGameState(screen, gs):
+
+click = False
+
+#TCA
+def main_menu():
+    mainClock = pygame.time.Clock()
+    screen = pygame.display.set_mode((400, 400), 0, 32)
+    font = pygame.font.SysFont("impact", 50)
+    click = False
+    while True:
+
+        screen.fill((0, 0, 0))
+        draw_text('Main Menu', font, (255, 255, 0), screen, 87, 35)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(100, 130, 200, 50)
+        button_2 = pygame.Rect(100, 230, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                game()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                quit_game()
+        pygame.draw.rect(screen, (255, 255, 255), button_1)
+        pygame.draw.rect(screen, (255, 255, 255), button_2)
+        draw_text("Play", font, (0, 0, 0), screen, 160, 122)
+        draw_text("Quit", font, (0, 0, 0), screen, 160, 223)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        mainClock.tick(60)
+         
+         
+def game():
+    mainClock = pygame.time.Clock()
+    screen = pygame.display.set_mode((400, 400), 0, 32)
+    gs = ChessEngine.GameState()
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        drawGameState(screen, gs)
+        pygame.display.update()
+        mainClock.tick(60)
+
+
+def quit_game():
+    pygame.quit()
+    quit()
+
+                     
+                     
+                     
+# TCA
+ def drawGameState(screen, gs):
      drawBoard(screen)
      drawPieces(screen, gs.board)
 
